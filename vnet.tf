957768dfef430c9c618fa5ae3b44a276d5ad8542
Stig Hegge <stig.hegge@gmail.com>
2020-09-10 13:11:40 +0000

    Added vnet module2

--- vnet.tf
+++ vnet.tf
@@ -1,10 +1,10 @@
-
 module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  source = "app.terraform.io/stigh-training/network/azurerm"
+  version = "2.0.0"
+  resource_group_name = ${var.prefix}-workshop
+  address_space       = "10.2.0.0/16"
+  subnet_prefixes     = ["10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24"]
+  subnet_names        = ["subnet3", "subnet4", "subnet5"]
 
   tags = {
     environment = "dev"
