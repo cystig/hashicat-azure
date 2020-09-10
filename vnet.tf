2f2a4e29374597ea2c6de2e55eec1d5d518e8b81
Stig Hegge <stig.hegge@gmail.com>
2020-09-10 13:17:32 +0000

    Added vnet module2

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,8 @@
+
 module "network" {
   source = "app.terraform.io/stigh-training/network/azurerm"
   version = "2.0.0"
+  location = "centralus"
   resource_group_name = stigh-workshop
   address_space       = "10.2.0.0/16"
   subnet_prefixes     = ["10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24"]
